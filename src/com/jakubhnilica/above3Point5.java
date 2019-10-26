a79bfcf62f5b01c5462af4747d3312be3522c47d
Jakub Hnilica <j.hnilica147@gmail.com>
2019-10-26 22:10:33 +0200

    removing of getter and setter which were moved into Vehicle class from above3Point5

--- src/com/jakubhnilica/above3Point5.java
+++ src/com/jakubhnilica/above3Point5.java
@@ -11,39 +11,5 @@ public class above3Point5 extends Vehicle {
         this.mileage = mileage;
     }
 
-    public void setFuel(TypeOfFuel fuel) { this.fuel = fuel; }
-
-    public void setPrice(double price) { this.price = price; }
-
-    public void setCategory(Category category) { this.category = category; }
-
-    public void setMileage (int mileage) { this.mileage = mileage; }
-
-    public void setYearOfProduction (int year) { this.yearOfProduction = year; }
-
-    public void setNumOfDoors (int numOfDoors) { this.numOfDoors = numOfDoors; }
-
-    public void numOfWheels (int numOfWheels) { this.numOfWheels = numOfWheels; }
-
-
-
-    public Category getCategory() {
-        return this.category;
-    }
-
-    public TypeOfFuel getFuel() {
-        return this.fuel;
-    }
-
-    public boolean getTypeOfAd(){ return this.typeOfAd; }
-
-    public double getPrice() { return this.price; }
-
-    public int getMileage() {return this.mileage; }
-
-    public int getYearOfProduction() { return this.yearOfProduction; }
-
-    public int getNumOfDoors() { return this.numOfDoors; }
-
-    public int getNumOfWheels() { return this.numOfWheels; }
 }
+
